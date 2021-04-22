72fd43e055936334a9f9658a9048254749854ae2
seojinsarejj <s19009@naver.com>
2021-04-22 10:37:12 +0900

    [ADD] Add other solution to [카카오 인턴] 수식 최대화.py

--- daily/[카카오 인턴] 수식 최대화.py
+++ daily/[카카오 인턴] 수식 최대화.py
@@ -48,4 +48,24 @@ def solution(expression):
     return result
 
 
-print(solution("100-200*300-500+20"))
+
+
+# 다른 사람의 풀이
+from itertools import permutations
+import re
+
+def other_solution(expression):
+    expressions = set(re.findall("\D", expression))
+    prior = permutations(expressions)
+    cand = []
+
+    for op_cand in prior:
+        temp = re.compile("(\D)").split('' + expression)
+        for exp in op_cand:
+            while exp in temp:
+                idx = temp.index(exp)
+                temp = temp[:idx-1] + [str(eval(''.join(temp[idx-1:idx+2])))] + temp[idx+2:]
+        cand.append(abs(int(temp[0])))
+    return max(cand)
+
+print(other_solution("100-200*300-500+20"))
