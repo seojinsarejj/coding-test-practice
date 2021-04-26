963afc1e4224af1b2e37472d928d46b0ff66e295
seojinsarejj <s19009@naver.com>
2021-04-26 15:21:51 +0900

    [ADD] Add 3차 to 단속카메라.py

--- daily/단속카메라.py
+++ daily/단속카메라.py
@@ -54,7 +54,38 @@ def solution(routes):
     return len(rngs)
             
                 
+# 3차 --- 50.0 / 100.0
+def solution(routes):
+    
+    routes.sort(key=lambda x : x[1])
+    
+    rngs = []
+    
+    
+    for route in routes :
         
+        r = range(route[0],route[1]+1)
+        
+        isIn = False
+        idx = 0
+        
+        for i in range(len(rngs)):
+            
+            if len(set(r) & set(range(rngs[i][0],rngs[i][1]+1))) == 0:
+                isIn = False
+            else:
+                isIn = True
+                idx = i
+                break
+                
+        if isIn:
+            rng = range(rngs[idx][0],rngs[idx][1]+1)
+            new_rng = list(set(r) & set(rng))
+            rngs[idx] = [min(new_rng),max(new_rng)]
+        else:
+            rngs.append(route)
+            
+    return len(rngs)
             
                 
 print(solution([[-20,15], [-14,-5], [-18,-13], [-5,-3]]))
