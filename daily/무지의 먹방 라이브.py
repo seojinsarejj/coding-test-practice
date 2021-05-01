876651d49d61bbcd41489a47e0f8f3c243d65203
seojinsarejj <s19009@naver.com>
2021-05-02 00:25:06 +0900

    [ADD] Add 2차 to 무지의 먹방 라이브.py

--- daily/무지의 먹방 라이브.py
+++ daily/무지의 먹방 라이브.py
@@ -1,40 +1,68 @@
-# 1차 --- 5.4 / 100.0
+# 1차 --- 24.1 / 100.0
+# def solution(food_times,k):
+    
+#     length = len(food_times)
+    
+#     index = 0
+#     count = 0
+    
+#     while True:
+        
+#         index += 1
+        
+#         if count >= k or sum(food_times) == 0:
+#             break
+    
+#         seq = index % length
+        
+#         if seq == 0:
+#             idx = -1
+#         else: 
+#             idx = seq - 1
+        
+#         if food_times[idx] == 0:
+#             continue
+            
+#         food_times[idx] -= 1
+#         count += 1
+        
+    
+#     if sum(food_times) == 0:
+#         return -1
+    
+#     seq = index % length
+
+#     if seq == 0:
+#         idx = -1
+#     else: 
+#         idx = seq - 1
+    
+#     return idx + 1
+
+
+# 2차 --- 28.1 / 100.0
 def solution(food_times,k):
-    
-    length = len(food_times)
-    
-    index = 0
+
+    index = -1
     count = 0
-    
+    length = len(food_times)
+
     while True:
-        
+
         index += 1
-        
+
         if count >= k or sum(food_times) == 0:
             break
-    
-        seq = index % length
-        
-        if seq == 0:
-            idx = -1
-        else: 
-            idx = seq - 1
-        
-        if food_times[idx] == 0:
+
+        if food_times[index%length] == 0:
             continue
-            
-        food_times[idx] -= 1
+
+        food_times[index%length] -= 1
         count += 1
-        
-    
+
+
     if sum(food_times) == 0:
         return -1
-    
-    seq = index % length
 
-    if seq == 0:
-        idx = -1
-    else: 
-        idx = seq - 1
+    return index%length + 1
     
-    return food_times[idx]
