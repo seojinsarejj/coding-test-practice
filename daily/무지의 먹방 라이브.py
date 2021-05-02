64856edb3f3d01a304ffb0ca08f06a5f14120835
seojinsarejj <s19009@naver.com>
2021-05-03 08:59:24 +0900

    [ADD] Add answer to 무지의 먹방 라이브.py

--- daily/무지의 먹방 라이브.py
+++ daily/무지의 먹방 라이브.py
@@ -41,28 +41,54 @@
 
 
 # 2차 --- 28.1 / 100.0
-def solution(food_times,k):
+# def solution(food_times,k):
 
-    index = -1
-    count = 0
-    length = len(food_times)
+#     index = -1
+#     count = 0
+#     length = len(food_times)
 
-    while True:
+#     while True:
 
-        index += 1
+#         index += 1
 
-        if count >= k or sum(food_times) == 0:
-            break
+#         if count >= k or sum(food_times) == 0:
+#             break
 
-        if food_times[index%length] == 0:
-            continue
+#         if food_times[index%length] == 0:
+#             continue
 
-        food_times[index%length] -= 1
-        count += 1
+#         food_times[index%length] -= 1
+#         count += 1
 
 
-    if sum(food_times) == 0:
+#     if sum(food_times) == 0:
+#         return -1
+
+#     return index%length + 1
+
+# 정답
+import heapq
+
+def solution(food_times, k):
+    
+    if sum(food_times) <= k:
         return -1
-
-    return index%length + 1
+    
+    queue = []
+    
+    for i,time in enumerate(food_times):
+        heapq.heappush(queue, (time,i))
+        
+    while True:
+        if queue[0][0] * len(queue) > k:
+            print(queue)
+            break
+        food = heapq.heappop(queue)
+        k -= food[0] * (len(queue) + 1)
+        
+    result = sorted(queue, key=lambda x : x[1])
+    print(result)
+    return result[k % len(queue)][0]
+    
+print(solution([3, 1, 2],5))
     
