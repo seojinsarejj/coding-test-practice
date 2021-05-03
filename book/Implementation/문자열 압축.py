ca80b64f577462b6f9e7a22bbde98dac310d0268
seojinsarejj <s19009@naver.com>
2021-05-03 11:05:24 +0900

    [CREAE] Create 문자열 압축.py

---
+++ book/Implementation/문자열 압축.py
@@ -0,0 +1,36 @@
+# 1차 --- 96.0 / 100.0
+def solution(s):
+
+   
+    answer = []
+
+    for i in range(1,len(s)//2+1):
+
+        stack = []
+        result = ""
+        idx = 0
+
+        while idx <= len(s):
+            
+            c = s[idx:idx+i]
+            idx += i
+
+            if stack != [] :
+                if stack[0] != c:
+                    if len(stack) == 1:
+                        result += stack[0]
+                    else:
+                        result += str(len(stack)) + stack[0]
+                    stack = []
+            
+            stack.append(c)
+        
+        
+        result += "".join(stack)
+            
+        answer.append(result)
+
+    return min([len(a) for a in answer])
+
+
+print(solution("abcabcabcabcdededededede"))
