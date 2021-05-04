56dab439a483a2eeb522bed891b5bea569412e90
seojinsarejj <s19009@naver.com>
2021-05-04 23:00:06 +0900

    [ADD] Add other solution to 최고의 집합.py

--- daily/최고의 집합.py
+++ daily/최고의 집합.py
@@ -15,3 +15,19 @@ def solution(n, s):
     return result
 
 
+# 다른 사람의 풀이
+def bestSet(n, s):
+    answer = []
+    a = int(s/n)
+
+    if a == 0:
+        return [-1]
+
+    b = s%n
+
+    for i in range(n-b):
+        answer.append(a)
+    for i in range(b):
+        answer.append(a+1)
+
+    return answer
