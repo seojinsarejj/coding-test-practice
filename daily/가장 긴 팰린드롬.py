678aa6238211c3f132562adcdf6f9a316e2781a2
seojinsarejj <s19009@naver.com>
2021-05-05 13:21:32 +0900

    [ADD] Add other solution to 가장 긴 팰린드롬.py

--- daily/가장 긴 팰린드롬.py
+++ daily/가장 긴 팰린드롬.py
@@ -1,7 +1,6 @@
-# 1차 --- 93.4 / 100.0
+# 1차 --- 성공
 def solution(s):
     
-
     for count in range(len(s),1,-1):
         
         for i in range(len(s)-count+1):
@@ -14,11 +13,13 @@ def solution(s):
                     palindrome = False
                     break
             
-            
             if palindrome == True:
                 return count
         
-        
     return 1
         
-        
+
+# 다른 사람의 풀이
+def longest_palindrom(s):
+    
+    return len(s) if s[::-1] == s else max(longest_palindrom(s[:-1]), longest_palindrom(s[1:]))
